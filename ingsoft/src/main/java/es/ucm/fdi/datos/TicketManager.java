79ba94e4b614bae3c4a07924b0a6a4b7d578a8fd
usuario_local <usuario_local@PTO0401>
2018-03-02 13:34:26 +0100

    cambios en ticketManager

--- ingsoft/src/main/java/es/ucm/fdi/datos/TicketManager.java
+++ ingsoft/src/main/java/es/ucm/fdi/datos/TicketManager.java
@@ -1,8 +1,17 @@
 package es.ucm.fdi.datos;
 
+import java.util.Map;
+
+import es.ucm.fdi.datos.dataobjects.Ticket;
+
 //OBSERVACTION: INDEPENDENT FROM DBMANAGER
 
 //Specified in CRC
 public class TicketManager {
+private Map <String, Ticket> availableTickets;
 
+void addTickets(/*params the ticket constructor needs*/){
+	Ticket t = new Ticket(//Cosas de ticket);
+	availableTickets.put(, t);
+}
 }
