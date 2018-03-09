f44b95492e33d512d6755817d9d3f1af255b8802
Borja Lozano <usuario_local@PTO0402>
2018-03-09 12:58:31 +0100

    Version 0.0.1
    - Added class TicketManager
    - Added class EmailSender

---
+++ ingsoft/src/main/java/es/ucm/fdi/TicketManagment/EmailSender.java
@@ -0,0 +1,57 @@
+package es.ucm.fdi.TicketManagment;
+
+/*Uses JavaMail API and Java Activation Framework (JAF) 
+* Download in:
+* http://www.oracle.com/technetwork/java/javasebusiness/downloads/java-archive-downloads-eeplat-419426.html#javamail-1.4.4-oth-JPR
+* http://www.oracle.com/technetwork/java/javasebusiness/downloads/java-archive-downloads-java-plat-419418.html#jaf-1.1.1-fcs-oth-JPR
+* After downloading go to Project->Properties->Java Build Path->Libraries->Add External Jar->Select the jar files (mail.jar and activation.jar) you have downloaded, then hit apply
+*/
+import java.io.UnsupportedEncodingException;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+
+public class EmailSender {
+
+    public static void send(String email, String text, String subject) {
+
+        final String username = "enjoymad@gmail.com";
+        final String password = "yourpassword";
+
+        Properties props = new Properties();
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+
+        Session session = Session.getInstance(props,
+          new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(username, password);
+            }
+          });
+
+        try {
+
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress("enjoymad@gmail.com"));
+            message.setRecipients(Message.RecipientType.TO,
+                InternetAddress.parse(email));
+            message.setSubject(subject);
+            message.setText(text);
+
+            Transport.send(message);
+
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ ingsoft/src/main/java/es/ucm/fdi/TicketManagment/TicketManager.java
@@ -0,0 +1,22 @@
+package es.ucm.fdi.TicketManagment;
+
+
+abstract class TicketManager {
+	public void buyTicket(String club_name, String user_name) {
+		ClubDAO clubs = new ClubDAO();
+		ClubPOJO club = clubs.getClub(club_name);
+		UserDAO users = new UserDAO();
+		UserPOJO user = users.getUser(user_name);
+		
+		float price = club.getPrice();
+		String email = user.getEmail();
+		
+		/*
+		 *  Payment code
+		 *  Stripe seems the best API to do it easily, it is not for free, it takes a 1.4% for each transaction + 0.25€
+		 */
+		
+		EmailSender.send(email, "Se ha completado la compra de una entrada de " + club_name, "Trámite EnjoyMad");
+	}
+}
+
