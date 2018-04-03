5cf77f94cfc43664d1dc4c35f68c14c0e8d19e9f
frblazqu <frblazqu@ucm.es>
2018-04-03 23:46:10 +0200

    Primeros desarrollos LocationFilter

--- ingsoft/src/main/java/es/ucm/fdi/business/SearchEngine/Filters/LocationFilter.java
+++ ingsoft/src/main/java/es/ucm/fdi/business/SearchEngine/Filters/LocationFilter.java
@@ -2,6 +2,7 @@ package es.ucm.fdi.business.SearchEngine.Filters;
 
 import es.ucm.fdi.business.data.FilterPOJO;
 import es.ucm.fdi.integration.data.ClubPOJO;
+import android.content.Context;
 import android.location.*;
 import org.json.simple.parser.*;
 
@@ -18,26 +19,14 @@ public class LocationFilter implements Filter{
 		
 		return null;
 	}
-	public GeoPoint getLocationFromAddress(String strAddress){
-
-		Geocoder coder = new Geocoder(this);
-		List<Address> address;
-		GeoPoint p1 = null;
-
-		try {
-		    address = coder.getFromLocationName(strAddress,5);
-		    if (address==null) {
-		       return null;
-		    }
-		    Address location=address.get(0);
-		    location.getLatitude();
-		    location.getLongitude();
-
-		    p1 = new GeoPoint((double) (location.getLatitude() * 1E6),
-		                      (double) (location.getLongitude() * 1E6));
-
-		    return p1;
-		    }
-		}
+	
+	//EN DESARROLLO!
+	public void getCoordenates()
+	{
+		/*LocationManager lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE); 
+		Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+		double longitude = location.getLongitude();
+		double latitude = location.getLatitude();*/
+	}
 
 }
