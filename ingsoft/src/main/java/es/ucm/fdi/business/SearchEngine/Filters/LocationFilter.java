14a243ee9a2775d69c3622136ea6eddc8534c765
frblazqu <frblazqu@ucm.es>
2018-04-03 23:37:10 +0200

    Dependencias y LocationFilter (SNAPSHOT)

--- ingsoft/src/main/java/es/ucm/fdi/business/SearchEngine/Filters/LocationFilter.java
+++ ingsoft/src/main/java/es/ucm/fdi/business/SearchEngine/Filters/LocationFilter.java
@@ -2,16 +2,42 @@ package es.ucm.fdi.business.SearchEngine.Filters;
 
 import es.ucm.fdi.business.data.FilterPOJO;
 import es.ucm.fdi.integration.data.ClubPOJO;
+import android.location.*;
+import org.json.simple.parser.*;
+
 
 public class LocationFilter implements Filter{
 
-	public boolean filter(ClubPOJO c) {
+	public boolean filter(ClubPOJO c) {	
+		
+		
 		return false;
 	}
 
 	public Object clone(FilterPOJO fp) {
-		// TODO Auto-generated method stub
+		
 		return null;
 	}
+	public GeoPoint getLocationFromAddress(String strAddress){
+
+		Geocoder coder = new Geocoder(this);
+		List<Address> address;
+		GeoPoint p1 = null;
+
+		try {
+		    address = coder.getFromLocationName(strAddress,5);
+		    if (address==null) {
+		       return null;
+		    }
+		    Address location=address.get(0);
+		    location.getLatitude();
+		    location.getLongitude();
+
+		    p1 = new GeoPoint((double) (location.getLatitude() * 1E6),
+		                      (double) (location.getLongitude() * 1E6));
+
+		    return p1;
+		    }
+		}
 
 }
