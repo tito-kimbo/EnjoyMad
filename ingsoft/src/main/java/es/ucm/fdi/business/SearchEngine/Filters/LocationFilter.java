983ca492c6dee7614e32f42f3d5173e1946f88d7
frblazqu <frblazqu@ucm.es>
2018-04-05 19:14:30 +0200

    GetLocationDevice implemented (review pending)

--- ingsoft/src/main/java/es/ucm/fdi/business/SearchEngine/Filters/LocationFilter.java
+++ ingsoft/src/main/java/es/ucm/fdi/business/SearchEngine/Filters/LocationFilter.java
@@ -6,9 +6,20 @@ import android.content.Context;
 import android.location.*;
 import org.json.simple.parser.*;
 
-
+/**
+ * This class is responsible of deciding whether a Club is near enough to satisfy the client search filter or not.
+ * 
+ * @author Francisco Javier Blázquez Martínez
+ */
 public class LocationFilter implements Filter{
 
+	private double deviceLatitude;
+	private double deviceLongitude;
+	private Context myContext;
+	
+	public LocationFilter(Context contexto)	{
+		myContext = contexto;
+	}
 	public boolean filter(ClubPOJO c) {	
 		
 		
@@ -20,15 +31,51 @@ public class LocationFilter implements Filter{
 		return null;
 	}
 	
-	//EN DESARROLLO!
-	public void getCoordinates()
+	
+	/**
+	 * Initializes {@link #deviceLatitude} and {@link #deviceLongitude} to the mobile device last known location.
+	 * 
+	 * @throws NullPointerException	In case the mobile phone has no location registers, so that it's not possible 
+	 * to get the last and nearest location of the device.
+	 */
+	private void getCoordinatesDevice() //EN DESARROLLO!
 	{
 		//NEEDS A CONTEXT -> Create a main Singleton base for the App with its context
 		
-		//LocationManager lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE); 
-		//Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-		//double longitude = location.getLongitude();
-		//double latitude = location.getLatitude();
+		//https://stackoverflow.com/questions/2227292/how-to-get-latitude-and-longitude-of-the-mobile-device-in-android
+		String context = Context.LOCATION_SERVICE;
+		LocationManager lm = (LocationManager)myContext.getSystemService(context); 
+		Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER); 
+	
+		if(location == null)
+			throw new NullPointerException("There is no last known location in this device.");
+		
+		deviceLongitude = location.getLongitude();
+		deviceLatitude  = location.getLatitude();
+		
+		//Suponemos de momento que inicializa correctamente la latitud y longitud a la actual del dispositivo.
 	}
-
+	
+	
+	
+	
+	
+	
+	/*
+	 * DUDAS PARA EDU Y OTROS:
+	 * 
+	 * -> Aclarar la forma de inicializar una variable de tipo LocationManager creando artificialmente un contexto.
+	 * -> Antes de presentar versión definitiva repasar la corrección del inglés de los javadocs (no me fío de mí).
+	 * 
+	 * 
+	 * 
+	 * OTRAS OBSERVACIONES:
+	 * 
+	 * -> El Context myContext no se inicializa en ningún momento, es algo propio del móvil que queda pendiente.
+	 *    (ver getCoordinatesDevice() y la inicialización de LocationManager lm).
+	 * -> Suponemos en esta clase inicializadas correctamente la latitud y longitud del ClubPOJO (cosa que no se hace).
+	 * 
+	 * 
+	 */
+	
 }
