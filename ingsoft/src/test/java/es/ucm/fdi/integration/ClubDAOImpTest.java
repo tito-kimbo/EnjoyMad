c11923bc06a78973078453f462644e1d67873a7e
CrlnBnnn <carlijnbonnen@correo.ugr.es>
2018-04-20 13:55:56 +0200

    Added ClubDAOImpTest to test functionality of the ClubDAOImp.

---
+++ ingsoft/src/test/java/es/ucm/fdi/integration/ClubDAOImpTest.java
@@ -0,0 +1,52 @@
+package es.ucm.fdi.integration;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+
+import org.junit.Test;
+
+import es.ucm.fdi.integration.data.ClubPOJO;
+
+/**
+ * This class tests the functionality of ClubDAOImp.
+ * @author Carlijn
+ */
+
+public class ClubDAOImpTest{
+	private static ClubPOJO club;
+	private static ClubDAO clubDao;
+	private static ArrayList<ClubPOJO> list = new ArrayList<ClubPOJO>();
+	
+	private static void createTestClubDAOImp(){
+		club = new ClubPOJO("id", "Kapital", "Calle Atocha, 125, 28012 Madrid", 17.0f, new HashSet<String>
+			(Arrays.asList("Electronica", "Reggaeton", "Funky", "R&B")));
+		
+		clubDao = new ClubDAOImp();
+		
+		list = new ArrayList<ClubPOJO>(Arrays.asList(club));
+		
+		clubDao.addClub(club);
+	}
+	
+	
+	@Test
+	public void testExist(){
+		createTestClubDAOImp();
+		assertEquals(clubDao.exist("id"),true);
+	}
+
+	@Test
+	public void testGetClub() {
+		createTestClubDAOImp();
+		assertEquals(clubDao.getClub("id"), club);
+	}
+	
+	@Test
+	public void testGetClubs() {
+		createTestClubDAOImp();
+		assertEquals(clubDao.getClubs(), list);
+	}
+}
