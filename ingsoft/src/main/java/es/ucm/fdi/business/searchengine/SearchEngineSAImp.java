322bf196ad33ef233e98df692891cd8d0ada4873
frblazqu <frblazqu@ucm.es>
2018-05-19 12:38:44 +0200

    SearchEngine and LocationFilter review.

--- ingsoft/src/main/java/es/ucm/fdi/business/searchengine/SearchEngineSAImp.java
+++ ingsoft/src/main/java/es/ucm/fdi/business/searchengine/SearchEngineSAImp.java
@@ -15,10 +15,8 @@ public class SearchEngineSAImp implements SearchEngineSA {
 	private ClubDAO clubAccess;
 
 	/**
-	 * @param clubAccess
-	 *            is the DAO to access the clubs
+	 * @param clubAccess is the DAO to access the clubs
 	 */
-
 	public SearchEngineSAImp(ClubDAO clubAccess) {
 		this.clubAccess = clubAccess;
 	}
@@ -26,21 +24,22 @@ public class SearchEngineSAImp implements SearchEngineSA {
 	/**
 	 * {@inheritDoc}
 	 */
-	public synchronized List<ElementHelper<ClubPOJO>> search(String words,
-			List<FilterPOJO> filters, UserPOJO usr) {
-		ElementHelper<ClubPOJO> aux;
+	public synchronized List<ElementHelper<ClubPOJO>> search(String words, List<FilterPOJO> filters, UserPOJO usr) {
+		
+		//To store the search result
 		List<ElementHelper<ClubPOJO>> searchResults = new ArrayList<ElementHelper<ClubPOJO>>();
-		List<ClubPOJO> clubs;
-		clubs = usr.getPreferencesList();
-
-		// Here we must find all the matching Data
+		//All the clubs in the sistem (where we will search)
+		List<ClubPOJO> clubs = usr.getPreferencesList();
+		ElementHelper<ClubPOJO> aux;
+		
+		//First we make sure that the club contains the word introduced by the user
 		for (ClubPOJO c : clubs) {
 			aux = new ElementHelper<ClubPOJO>(c);
-			aux.setVisible(
-					c.getCommercialName().toLowerCase().contains(words.toLowerCase()));
+			aux.setVisible(c.getCommercialName().toLowerCase().contains(words.toLowerCase()));
 			searchResults.add(aux);
 		}
-
+		
+		//Then whe check which clubs are a match for the given filters
 		for (FilterPOJO f : filters) {
 			FilterStrategy currentFilter = FilterMapper.mapFilter(f);
 
