01b4e0bca10c4d9117edab372c5c3676fed24e8c
frblazqu <frblazqu@ucm.es>
2018-05-19 13:11:33 +0200

    No filter search passed.

--- ingsoft/src/test/java/es/ucm/fdi/bussines/searchengine/SearchEngineSAImpTest.java
+++ ingsoft/src/test/java/es/ucm/fdi/bussines/searchengine/SearchEngineSAImpTest.java
@@ -1,5 +1,7 @@
 package es.ucm.fdi.bussines.searchengine;
 
+import static org.junit.Assert.*;
+
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.ArrayList;
@@ -8,6 +10,7 @@ import java.util.Set;
 import java.util.HashSet;
 
 import org.junit.Test;
+import org.junit.Assert.*;
 
 import es.ucm.fdi.business.data.FilterPOJO;
 import es.ucm.fdi.business.data.TagPOJO;
@@ -17,28 +20,39 @@ import es.ucm.fdi.integration.data.UserPOJO;
 import es.ucm.fdi.business.searchengine.SearchEngineSAImp;
 import es.ucm.fdi.business.util.ElementHelper;;
 
+
 public class SearchEngineSAImpTest {
 	private static ClubDAOImp cd;
-
+	private static SearchEngineSAImp searchMotor;
+	
 	/**
 	 * This initializes the ClubDao the first time the class is created.
 	 */
-	static {
+	static 
+	{
 		cd = new ClubDAOImp();
 
-		Set<TagPOJO> l1 = new HashSet<TagPOJO>(); // Club's tags (empty)
-		ClubPOJO c = new ClubPOJO("a2868521eac27ed8c7c4d7a38051f912", "Teatro Kapital",
-				"C/Falsa 1234", 20.30F, l1);
-
-		cd.addClub(c);
+		Set<TagPOJO> l1 = new HashSet<TagPOJO>(); // Club1's tags (empty)
+		Set<TagPOJO> l2 = new HashSet<TagPOJO>(); // Club2's tags (empty)
+		
+		ClubPOJO c1 = new ClubPOJO("Club1", "Teatro Kapital", "C/Falsa 1", 20.30F, l1);
+		ClubPOJO c2 = new ClubPOJO("Club2", "Mitty", "C/Falsa 2", 15.00F, l2);
+		
+		cd.addClub(c1);
+		cd.addClub(c2);
+		
+		searchMotor = new SearchEngineSAImp(cd);
 	}
 
 	/**
 	 * This method returns an user ready to execute searchs.
 	 */
-	public static UserPOJO readyToSearchUser(String id, String user, String name) {
-		UserPOJO usr = new UserPOJO(id, user, "password", "email@mail.com", name,
-				LocalDate.of(2018, Month.JANUARY, 1));
+	public static UserPOJO readyToSearchUser(String id, String user, String name) 
+	{
+		LocalDate birthday = LocalDate.of(2018, Month.JANUARY, 1);
+		
+		UserPOJO usr = new UserPOJO(id, user, "password", "email@mail.com", name, birthday);
+		
 		usr.setPreferencesList(cd.getClubs());
 
 		return usr;
@@ -48,11 +62,14 @@ public class SearchEngineSAImpTest {
 	 * TEST 1: Search without filters.
 	 */
 	@Test
-	public void noFilterSearchTest() {
-		List<FilterPOJO> filters = new ArrayList<FilterPOJO>(); // Filters
-																// (empty)
+	public void noFilterSearchTest() 
+	{
+		List<FilterPOJO> filters = new ArrayList<FilterPOJO>(); // Filters (empty)
+		
 		UserPOJO usr = readyToSearchUser("id", "frblazqu", "Francis");
 
-		//List<ElementHelper<ClubPOJO>> aux = search("", filters, usr);
+		List<ElementHelper<ClubPOJO>> aux = searchMotor.search("", filters, usr);
+		
+		assertTrue("No filterSearch failed", aux.size()== 2);
 	}
 }
