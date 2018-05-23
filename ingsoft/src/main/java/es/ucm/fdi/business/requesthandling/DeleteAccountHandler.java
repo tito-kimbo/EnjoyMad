32d64ceae5b84e36e376e6dc76feab698fb1688f
CrlnBnnn <carlijnbonnen@correo.ugr.es>
2018-05-23 15:17:11 +0200

    Completed DeleteAccountHandler

--- ingsoft/src/main/java/es/ucm/fdi/business/requesthandling/DeleteAccountHandler.java
+++ ingsoft/src/main/java/es/ucm/fdi/business/requesthandling/DeleteAccountHandler.java
@@ -1,6 +1,11 @@
 package es.ucm.fdi.business.requesthandling;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+
 import es.ucm.fdi.business.FrontController;
+import es.ucm.fdi.business.data.AnswerPOJO;
 import es.ucm.fdi.business.data.RequestPOJO;
 
 public class DeleteAccountHandler implements RequestHandler {
@@ -20,9 +25,28 @@ public class DeleteAccountHandler implements RequestHandler {
 	public void run(){
 		//Parse request data
 		
+		String userID;
+		List<Object> answerData;
+		
+		userID = (String)rp.getParameters().get(0);
+				
+		answerData = new ArrayList<Object>();
+
 		//	Call relevant ProfileManagerSA methods
 		
-		//	Answer
+		try{
+			fc.getProfileManagerSA().removeUser(userID);
+			answerData.add(true);
+		}catch(NoSuchElementException nsee){
+			System.out.println("Invalid user on delete account" + nsee.getMessage());
+			answerData.add(false);
+		}
+		
+		finally{
+			//	Answer
+			fc.addAnswer(rp.getID(), new AnswerPOJO(answerData));
+		}
+		
 	}
 	
 	public Object clone(RequestPOJO rp){
