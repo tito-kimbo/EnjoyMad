bf85256b0b49a38c875d7f90173345a1883e5bf6
Pablo Hernández <pablohernandezaguado@gmail.com>
2018-05-25 22:34:31 +0200

    RemoveProfile use cases

--- ingsoft/src/test/java/es/ucm/fdi/usecases/RemoveProfile.java
+++ ingsoft/src/test/java/es/ucm/fdi/usecases/RemoveProfile.java
@@ -1,7 +1,6 @@
 package es.ucm.fdi.usecases;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
@@ -14,7 +13,6 @@ import java.util.List;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.mindrot.jbcrypt.BCrypt;
 
 import es.ucm.fdi.business.FrontController;
 import es.ucm.fdi.business.ProductionConfig;
@@ -29,16 +27,14 @@ import es.ucm.fdi.integration.data.UserPOJO;
  * The use case executes as follows
  * 
  * 1.   request() method from FrontController is called with respective
- *      requests of Club and User modification.
+ *      requests of Club and User removal.
  * 		    * FrontController handles request internally on separate thread.
  * 2.   While there is no answer, polling (calls poll()) at fixed time intervals.
  * 3.   Valid answer is given at some point.
  */
-public class ModifyProfile {
+public class RemoveProfile {
     public static final int POLLING_DELAY = 100;
-    private static String customClubID = "idModify Club240520189229";
-    private static String customUserID = "idModify User245920780023";
-    private static String hashedPassword = BCrypt.hashpw("pass1234", BCrypt.gensalt());
+    private static String customRemoveID = "idRemove 2477530189229";
     
     public static TagPOJO[] tags = {
             new TagPOJO("pop"), new TagPOJO("edm"), new TagPOJO("latino"),
@@ -52,24 +48,11 @@ public class ModifyProfile {
                     new HashSet<TagPOJO>(Arrays.asList(tags[7], tags[8], tags[0]))
                 );
 
-    public static ClubPOJO clubChanges =
-            new ClubPOJO(
-                    "club08", "El fresquito", null, 7.50f,
-                    new HashSet<TagPOJO>(Arrays.asList(tags[0], tags[2], tags[4]))
-                );
-
     public static UserPOJO existingUser = 
             new UserPOJO(
                     "user02", "alfonrom2", "dfsd653;^34;dqwe", 
                     "alf.rom15@mail.es", "Alfonso Romo", 
                     LocalDate.parse("1997-05-14")
-            );
-
-    public static UserPOJO userChanges = 
-            new UserPOJO(
-                    "user02", "alfonbest3", hashedPassword, 
-                    "alfonsoromo@gmail.com", "Alfonso Romo Hernán", 
-                    null
             );
 
     FrontController fc;
@@ -107,24 +90,17 @@ public class ModifyProfile {
     }    
 
     // Uses customID
-    private RequestPOJO buildOneClubRP(ClubPOJO club) {
+    private RequestPOJO buildDeleteRP(String id) {
         List<Object> l = new ArrayList<Object>();
-        l.add(club);
-        return new RequestPOJO(customClubID, new RequestPOJO("", RequestType.MODIFY_CLUB, l));
-    }
-
-    // Uses customID
-    private RequestPOJO buildOneUserRP(UserPOJO user) {
-        List<Object> l = new ArrayList<Object>();
-        l.add(user);
-        return new RequestPOJO(customUserID, new RequestPOJO("", RequestType.MODIFY_USER, l));
+        l.add(id);
+        return new RequestPOJO(customRemoveID, new RequestPOJO("", RequestType.DELETE_ACCOUNT, l));
     }
 
     @Test
-    public void modifyUserProfileTest() {
+    public void removeUserProfileTest() {
         // Build Request
         AnswerPOJO ans;        
-        RequestPOJO rp = buildOneUserRP(userChanges);
+        RequestPOJO rp = buildDeleteRP(existingUser.getID());
         
         // Do request to sv
         String id = fc.request(rp);
@@ -146,49 +122,18 @@ public class ModifyProfile {
         );
 
         // Check integrity of changes
-        UserPOJO modifiedUser = fc.getProfileManagerSA().getUser(userChanges.getID());
-        assertNotNull(
-            "ERROR -> User not found in DAO.", 
+        UserPOJO modifiedUser = fc.getProfileManagerSA().getUser(existingUser.getID());
+        assertNull(
+            "ERROR -> User found in DAO.", 
             modifiedUser
         );
-
-        if (userChanges.getUsername() != null) {
-            assertEquals(
-                "ERROR -> Unchanged username.",
-                modifiedUser.getUsername(), userChanges.getUsername()
-            );
-        }
-        if (userChanges.getHashedPassword() != null) {
-            assertEquals(
-                "ERROR -> Unchanged password.",
-                modifiedUser.getHashedPassword(), userChanges.getHashedPassword()
-            );
-        }
-        if (userChanges.getEmail() != null) {
-            assertEquals(
-                "ERROR -> Unchanged email.",
-                modifiedUser.getEmail(), userChanges.getEmail()
-            );
-        }
-        if (userChanges.getName() != null) {
-            assertEquals(
-                "ERROR -> Unchanged name.",
-                modifiedUser.getName(), userChanges.getName()
-            );
-        }
-        if (userChanges.getBirthday() != null) {
-            assertEquals(
-                "ERROR -> Unchanged birthday.",
-                modifiedUser.getBirthday(), userChanges.getBirthday()
-            );
-        }
     }
 
     @Test
-    public void modifyClubProfileTest() {
+    public void removeClubProfileTest() {
         // Build Request
         AnswerPOJO ans;        
-        RequestPOJO rp = buildOneClubRP(clubChanges);
+        RequestPOJO rp = buildDeleteRP(existingClub.getID());
         
         // Do request to sv
         String id = fc.request(rp);
@@ -210,36 +155,10 @@ public class ModifyProfile {
         );
 
         // Check integrity of changes
-        ClubPOJO modifiedClub = fc.getProfileManagerSA().getClub(clubChanges.getID());
-        assertNotNull(
-            "ERROR -> Club not found in DAO.", 
+        ClubPOJO modifiedClub = fc.getProfileManagerSA().getClub(existingClub.getID());
+        assertNull(
+            "ERROR -> Club found in DAO.", 
             modifiedClub
         );
-
-        if (clubChanges.getCommercialName() != null) {
-            assertEquals(
-                "ERROR -> Unchanged commercial name.",
-                modifiedClub.getCommercialName(), clubChanges.getCommercialName()
-            );
-        }
-        if (clubChanges.getAddress() != null) {
-            assertEquals(
-                "ERROR -> Unchanged address.",
-                modifiedClub.getAddress(), clubChanges.getAddress()
-            );
-        }
-        if (clubChanges.getPrice() != ClubPOJO.PRICE_NULL) {
-            assertEquals(
-                "ERROR -> Unchanged price.",
-                modifiedClub.getPrice(), clubChanges.getPrice(),
-                0.010f
-            );
-        }
-        if (clubChanges.getTags() != null) {
-            assertEquals(
-                "ERROR -> Unchanged tags.",
-                modifiedClub.getTags(), clubChanges.getTags()
-            );
-        }
     }
 }
