4d91599d7fb5466e0fd01a99d37c507e39790f00
frblazqu <frblazqu@ucm.es>
2018-05-25 13:44:01 +0200

    ModifyUserHandler test working.

--- ingsoft/src/test/java/es/ucm/fdi/business/requesthandling/ModifyUserHandlerTest.java
+++ ingsoft/src/test/java/es/ucm/fdi/business/requesthandling/ModifyUserHandlerTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import es.ucm.fdi.business.FrontController;
 import es.ucm.fdi.business.ProductionConfig;
+import es.ucm.fdi.business.data.AnswerPOJO;
 import es.ucm.fdi.business.data.RequestPOJO;
 import es.ucm.fdi.business.requesthandling.tools.RequestType;
 import es.ucm.fdi.integration.data.UserPOJO;
@@ -24,29 +25,45 @@ public class ModifyUserHandlerTest {
 	private static int THREAD_LIMIT = 100;
 
 	// Uses customID
-	private RequestPOJO buildModifyUserRP(UserPOJO user)
+	private RequestPOJO buildModifyUserRP(String userId, UserPOJO user)
 	{
 		List<Object> l = new ArrayList<Object>();
+		l.add(userId);
 		l.add(user);
 		
 		return new RequestPOJO(customID, new RequestPOJO(RequestType.MODIFY_USER, l));
 	}
 	
+	/**
+	 * Test to prove that the way to modify users using request works properly.
+	 */
 	@Test
 	public void modifyUserTest() 
 	{
+		//We initialize the FrontController
 		Initializer.initialize();
 		FrontController fc = ProductionConfig.getFrontController();
 		
-		UserPOJO user = new UserPOJO("Id", "user", "password", "email@mail.com", "Francis", LocalDate.of(2018, Month.JANUARY, 1));
+		//The system should have at least one user to modify it, we add the user
+		UserPOJO user = new UserPOJO("Id", "user", "password", "email@mail.com", 
+				"Francis Blázquez", LocalDate.of(1998, Month.JANUARY, 1));
 		
 		fc.getProfileManagerSA().addNewUser(user);
 		
-		UserPOJO modifyUser = new UserPOJO(null, null, "0000", null, null, null);
+		//Then we create the RequestPOJO to ask the system to modify the user
+		UserPOJO modifyUser = new UserPOJO("Id", "titoKimbo", null, null, null, null);
 		
-		RequestPOJO rp = buildModifyUserRP(modifyUser);
+		RequestPOJO rp = buildModifyUserRP(modifyUser.getID(), modifyUser);
 		
-		fc.request(rp);
+		ModifyUserHandler handler = new ModifyUserHandler(fc, rp);
+		
+		//We execute the request
+		handler.run();
+		
+		//We check that the system processed the request and did it successfully.
+		assertTrue("Modifying the user failed.", (Boolean) fc.poll(customID).getAnswer().get(0));
+		assertEquals("Modifying the user failed.", 
+				(String) fc.getProfileManagerSA().getUser("Id").getUsername(), "titoKimbo");
 	}
 		
 	
